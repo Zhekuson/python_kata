20e4da992f7d69633fe64391b2545014d52a25af
Zhekuson <zhekuson@gmail.com>
2022-08-01 22:58:16 +0300

    clean python  ch 2 added

---
+++ clean_python/chapter2/examples.py
@@ -0,0 +1,9 @@
+def apply_discount(product: dict, discount: float):
+    price = int(product['price'] * (1.0 - discount))
+    assert 0 <= price <= product['price']
+    return price
+
+
+if __name__ == "__main__":
+    print(apply_discount({'name': 'apple', 'price': 10}, 0.3))
+    print(apply_discount({'name': 'apple', 'price': 10}, 2))
