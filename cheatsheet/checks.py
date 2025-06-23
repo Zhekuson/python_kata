e785c4ef59d0efe1bca5d7cfc985164dc3225de4
zhekuson <zhekuson@gmail.com>
2025-06-23 19:23:08 +0300

    some examples

---
+++ cheatsheet/checks.py
@@ -0,0 +1,12 @@
+a = {1:1, 2:2, 3:"b"}
+b = {1:1, 2:2, 3:"b"}
+c = a
+print("a =", a)
+print("b =", b)
+print("link(a) c =", c)
+print("a == b", a == b)
+print("a is b", a is b)
+print("a == c", a == c)
+print("a is c", a is c)
+print("b == c", b == c)
+print("b is c", b is c)
